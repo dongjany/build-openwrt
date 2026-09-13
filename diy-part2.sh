8cc3378d7aacc20526180ef01bbeb05cc125a22b
dongjany <3145255636@qq.com>
2026-09-13 13:29:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,8 @@
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.5.55/g' package/base-files/files/bin/config_generate
 
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
