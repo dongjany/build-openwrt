d201b7f0ad31f63f6170a13d9f57874f4d4859b0
dongjany <3145255636@qq.com>
2026-09-13 14:31:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,5 +4,8 @@ wget -P package/network/utils/nftables/patches https://raw.githubusercontent.com
 # 下载 libnftnl fullcone 配套补丁
 wget -P package/libs/libnftnl/patches https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/libs/libnftnl/patches/001-libnftnl-add-fullcone-expression-support.patch
 
+
+sed -i 's/PKG_RELEASE:=$(AUTORELEASE)/PKG_RELEASE:=1/g' package/network/utils/fullconenat-nft/Makefile
+
 # Add a feed source
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
