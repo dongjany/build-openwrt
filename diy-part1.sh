75be0f2f61d6a941e4e09a30c876ecca601fefb6
dongjany <3145255636@qq.com>
2026-09-14 22:35:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,14 @@
-rm -rf package/network/config/firewall4
-rm -rf package/network/utils/nftables
-rm -rf package/libs/libnftnl
-rm -rf package/network/utils/iptables
+rm -rf package/network/config/firewall4 package/network/utils/nftables package/libs/libnftnl
 
-# svn 拉取github指定目录
-svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-25.12/package/network/config/firewall4 package/network/config/firewall4
-svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-25.12/package/network/utils/nftables package/network/utils/nftables
-svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-25.12/package/libs/libnftnl package/libs/libnftnl
-svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-25.12/package/network/utils/iptables package/network/utils/iptables
+# 拉 immortalwrt
+git clone --depth=1 --branch openwrt-25.12 --filter=blob:none --sparse https://github.com/immortalwrt/immortalwrt tmp_imm
+cd tmp_imm
+git sparse-checkout set package/network/config/firewall4 package/network/utils/nftables package/libs/libnftnl
+cd ..
+cp -r tmp_imm/package/network/config/firewall4 package/network/config/
+cp -r tmp_imm/package/network/utils/nftables package/network/utils/
+cp -r tmp_imm/package/libs/libnftnl package/libs/
+rm -rf tmp_imm
 
 # 修复 fullconenat-nft 报错
 sed -i 's/PKG_RELEASE:=$(AUTORELEASE)/PKG_RELEASE:=3/g' package/network/utils/fullconenat-nft/Makefile
