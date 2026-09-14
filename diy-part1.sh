510f6e02d6da7ccabc2a55347b82af46660d3d6a
dongjany <3145255636@qq.com>
2026-09-14 22:51:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,9 @@ git_clone_path() {
     rm -rf "${TMPDIR}"
 }
 
-# ========== 替换 firewall4 nftables libnftnl fullconenat-nft ==========
-rm -rf package/network/config/firewall4 package/network/utils/nftables package/libs/libnftnl package/network/utils/fullconenat-nft
-git_clone_path openwrt-25.12 https://github.com/immortalwrt/immortalwrt package/network/config/firewall4 package/network/utils/nftables package/libs/libnftnl package/network/utils/fullconenat-nft
+# ========== 替换 firewall4 nftables libnftnl iptables fullconenat-nft ==========
+rm -rf package/network/config/firewall4 package/network/utils/nftables package/libs/libnftnl package/network/utils/iptables package/network/utils/fullconenat-nft
+git_clone_path openwrt-25.12 https://github.com/immortalwrt/immortalwrt package/network/config/firewall4 package/network/utils/nftables package/libs/libnftnl package/network/utils/iptables package/network/utils/fullconenat-nft
 
 # Add a feed source
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
