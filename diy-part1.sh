1c26f22761d8f888d117bc75e8dd28a6cd8a87d6
dongjany <3145255636@qq.com>
2026-09-14 18:46:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,13 @@
 rm -rf package/network/config/firewall4
 rm -rf package/network/utils/nftables
 rm -rf package/libs/libnftnl
+rm -rf package/network/utils/iptables
 
 # svn 拉取github指定目录
 svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-25.12/package/network/config/firewall4 package/network/config/firewall4
 svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-25.12/package/network/utils/nftables package/network/utils/nftables
 svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-25.12/package/libs/libnftnl package/libs/libnftnl
+svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-25.12/package/network/utils/iptables package/network/utils/iptables
 
 # 修复 fullconenat-nft 报错
 sed -i 's/PKG_RELEASE:=$(AUTORELEASE)/PKG_RELEASE:=1/g' package/network/utils/fullconenat-nft/Makefile
