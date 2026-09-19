4f3993d6a63aa2120d156d986b627883cf23d8bf
dongjany <3145255636@qq.com>
2026-09-19 09:01:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+#========================================================================================================================
+# https://github.com/dongjany/build-openwrt
+# Description: Automatically Build OpenWrt
+# Function: DIY script (Before updating feeds — modify the default IP, hostname, theme, add/remove packages, etc.)
+# Source code repository: https://github.com/Lienol/openwrt / Branch: 23.05
+#========================================================================================================================
 
+# Add a custom feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-# Add a feed source
+# Remove unnecessary packages
+# rm -rf package/utils/{ucode,fbtest}
 git clone --depth 1 --branch v2.4.3 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
