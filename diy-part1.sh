8c24faeadcc73720db29aac0e0ff17fcb0e62e77
dongjany <3145255636@qq.com>
2026-09-19 12:41:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # DIY‑PART1: feeds update 之前执行 | Lienol‑OpenWrt 25.12
 set -euo pipefail
-cd openwrt
-
-cp feeds.conf.default feeds.conf.default.bak
-# 国内镜像加速源，需要取消注释
-# sed -i 's|git.openwrt.org/openwrt|github.com/openwrt|g' feeds.conf.default
 
 # 追加自定义feed源
 # echo "src-git custom_feed https://github.com/xxx/xxx.git" >> feeds.conf.default
