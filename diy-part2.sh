173f30f9783aead2f1b7b79e2d10d848871ac98c
dongjany <3145255636@qq.com>
2026-09-19 09:06:54 +0800

    Update diy-part2.sh with script metadata
    Added comments for script purpose and source repository.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#========================================================================================================================
+# https://github.com/dongjany/build-openwrt
+# Description: Automatically Build OpenWrt
+# Function: DIY script (Before updating feeds — modify the default IP, hostname, theme, add/remove packages, etc.)
+# Source code repository: https://github.com/Lienol/openwrt / Branch: 23.05
+#========================================================================================================================
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.55/g' package/base-files/files/bin/config_generate
